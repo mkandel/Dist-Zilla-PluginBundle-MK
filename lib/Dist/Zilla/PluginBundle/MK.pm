198e483d146587fea923f7fde7b82ca1b29708c4
mkandel <marc.kandel.cpan@gmail.com>
2013-05-09 18:03:52 -0700

    initial commit

---
+++ lib/Dist/Zilla/PluginBundle/MK.pm
@@ -0,0 +1,172 @@
+package Dist::Zilla::PluginBundle::RJBS;
+# ABSTRACT: BeLike::RJBS when you build your dists
+
+use Moose;
+use Moose::Autobox;
+use Dist::Zilla 2.100922; # TestRelease
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+=head1 DESCRIPTION
+
+This is the plugin bundle that RJBS uses.  It is more or less equivalent to:
+
+  [Git::GatherDir]
+  [@Basic]
+  ; ...but without GatherDir and ExtraTests
+
+  [AutoPrereqs]
+  [Git::NextVersion]
+  [PkgVersion]
+  [MetaConfig]
+  [MetaJSON]
+  [NextRelease]
+
+  [Test::ChangesHasContent]
+  [PodSyntaxTests]
+  [Test::Compile]
+  [ReportVersions::Tiny]
+
+  [PodWeaver]
+  config_plugin = @RJBS
+
+  [GithubMeta]
+  user = rjbs
+  remote = github
+  remote = origin
+
+  [@Git]
+  tag_format = %v
+
+If the C<task> argument is given to the bundle, PodWeaver is replaced with
+TaskWeaver and Git::NextVersion is replaced with AutoVersion.  If the
+C<manual_version> argument is given, AutoVersion is omitted.
+
+If the C<github_issues> argument is given, and true, the F<META.*> files will
+point to GitHub issues for the dist's bugtracker.
+
+=cut
+
+use Dist::Zilla::PluginBundle::Basic;
+use Dist::Zilla::PluginBundle::Filter;
+use Dist::Zilla::PluginBundle::Git;
+
+has manual_version => (
+  is      => 'ro',
+  isa     => 'Bool',
+  lazy    => 1,
+  default => sub { $_[0]->payload->{manual_version} },
+);
+
+has major_version => (
+  is      => 'ro',
+  isa     => 'Int',
+  lazy    => 1,
+  default => sub { $_[0]->payload->{version} || 0 },
+);
+
+has is_task => (
+  is      => 'ro',
+  isa     => 'Bool',
+  lazy    => 1,
+  default => sub { $_[0]->payload->{task} },
+);
+
+has github_issues => (
+  is      => 'ro',
+  isa     => 'Bool',
+  lazy    => 1,
+  default => sub { $_[0]->payload->{github_issues} },
+);
+
+has weaver_config => (
+  is      => 'ro',
+  isa     => 'Str',
+  lazy    => 1,
+  default => sub { $_[0]->payload->{weaver_config} || '@RJBS' },
+);
+
+sub configure {
+  my ($self) = @_;
+
+  $self->log_fatal("you must not specify both weaver_config and is_task")
+    if $self->is_task and $self->weaver_config ne '@RJBS';
+
+  $self->add_plugins('Git::GatherDir');
+  $self->add_plugins('CheckPrereqsIndexed');
+  $self->add_plugins('CheckExtraTests');
+  $self->add_bundle('@Filter', {
+    '-bundle' => '@Basic',
+    '-remove' => [ 'GatherDir', 'ExtraTests' ],
+  });
+
+  $self->add_plugins('AutoPrereqs');
+
+  unless ($self->manual_version) {
+    if ($self->is_task) {
+      my $v_format = q<{{cldr('yyyyMMdd')}}>
+                   . sprintf('.%03u', ($ENV{N} || 0));
+
+      $self->add_plugins([
+        AutoVersion => {
+          major     => $self->major_version,
+          format    => $v_format,
+          time_zone => 'America/New_York',
+        }
+      ]);
+    } else {
+      $self->add_plugins([
+        'Git::NextVersion' => {
+          version_regexp => '^([0-9]+\.[0-9]+)$',
+        }
+      ]);
+    }
+  }
+
+  $self->add_plugins(qw(
+    PkgVersion
+    MetaConfig
+    MetaJSON
+    NextRelease
+    Test::ChangesHasContent
+    PodSyntaxTests
+    Test::Compile
+    ReportVersions::Tiny
+  ));
+
+  $self->add_plugins(
+    [ Prereqs => 'TestMoreWithSubtests' => {
+      -phase => 'test',
+      -type  => 'requires',
+      'Test::More' => '0.96'
+    } ],
+  );
+
+  if ($self->is_task) {
+    $self->add_plugins('TaskWeaver');
+  } else {
+    $self->add_plugins([
+      PodWeaver => { config_plugin => $self->weaver_config }
+    ]);
+  }
+
+  $self->add_plugins(
+    [ GithubMeta => {
+      user   => 'rjbs',
+      remote => [ qw(github origin) ],
+      issues => $self->github_issues,
+    } ],
+  );
+
+  $self->add_bundle('@Git' => {
+    tag_format => '%v',
+    remotes_must_exist => 0,
+    push_to    => [
+      'origin :',
+      'github :',
+    ],
+  });
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
